fd12eca9d1aaf2a08954ba3a8ce50c9029df916d
Paolo Tiu <paolotiu17@gmail.com>
2021-02-17 17:08:22 +0800

    test route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,20 @@
 import { receiveFile } from "controllers/multerTest";
 import { youtube } from "controllers/youtubeDl";
 import express from "express";
+import createHttpError from "http-errors";
+import { Server } from "socket.io";
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Hello World");
+router.post("/", (req, res, next) => {
+  const { socketId } = req.body;
+  if (!socketId) {
+    return next(createHttpError(400, " socketId needed"));
+  }
+  const io = req.app.get("io") as Server;
+  const sockets = req.app.get("sockets") as { [key: string]: string };
+  io.to(sockets[socketId]).emit("pong");
+  console.log(req.app.get("sockets"));
+  res.json("hey");
 });
 router.get("/yt", youtube);
 router.post("/vid", receiveFile);
