6429d2417605c3d0dd45d11e928079779be8cfaf
Paolo Tiu <paolotiu17@gmail.com>
2021-03-03 17:47:35 +0800

    Added event emition

--- src/controllers/youtubeDl.ts
+++ src/controllers/youtubeDl.ts
@@ -3,15 +3,29 @@ import fs from "fs";
 import { v4 as uuid } from "uuid";
 import createHttpError from "http-errors";
 import progress from "progress-stream";
-import { setDirFileCountInterval } from "util/setDirFileCountInterval";
+import { emitFileCountStatus, setDirFileCountInterval } from "util/setDirFileCountInterval";
 import { createCanvasLines } from "util/createCanvasLines";
 import { extractVideoFrames } from "util/extractVideoFrames";
 import { getColorFromFiles } from "util/getMultipleColors";
 import { sortFiles } from "util/sortFiles";
 import ytdl from "ytdl-core";
+import { getSocket } from "util/getSocket";
 
 export const youtube: RequestHandler = async (req, res, next) => {
-  const { url } = req.body;
+  const { url, socketId } = req.body as { socketId: string; url: string };
+
+  // Check if socketId exists
+  if (!socketId) {
+    return next(createHttpError(400, "Socket ID needed"));
+  }
+
+  // Get client socket
+  const emitToClient = getSocket(req, socketId);
+
+  // Validation check
+  if (!emitToClient) {
+    return next(createHttpError(400, "No socket with that id"));
+  }
 
   // File path
   const id = uuid();
@@ -38,8 +52,12 @@ export const youtube: RequestHandler = async (req, res, next) => {
   const str = progress({
     time: 1000 /* every 1s */,
   });
+
+  // Youtube DL Progress
   str.on("progress", function (progress) {
-    console.log(progress);
+    emitToClient("status", {
+      message: "Downloading",
+    });
   });
 
   // Readstream to monitor the file
@@ -47,27 +65,40 @@ export const youtube: RequestHandler = async (req, res, next) => {
 
   stream.on("finish", async () => {
     console.log("Downlaod finished");
+
+    // Create dir file count listener
+    const clearDirInvteral = await setDirFileCountInterval(
+      FRAMES_DIR,
+      emitFileCountStatus(emitToClient),
+      1000
+    );
+
     try {
-      const clearDirInvteral = await setDirFileCountInterval(
-        FRAMES_DIR,
-        (files) => {
-          console.log(files.length);
-        },
-        1000
-      );
+      // Extract video frames
+      await extractVideoFrames(VID_FILE, FRAMES_DIR, { isYoutube: true });
 
-      await extractVideoFrames(VID_FILE, FRAMES_DIR);
+      // Manually emit that frame extraction is done
+      emitFileCountStatus(emitToClient)(1000);
 
+      // Clear listener after extraction
       clearDirInvteral();
+
       // Get the files then sort
       const files = fs.readdirSync(FRAMES_DIR);
       const sorted = sortFiles(files, FRAMES_DIR);
+
       // Returns an array of tuples which are [<red>, <green>, <blue>]
       const colors = await getColorFromFiles(sorted.map((file) => FRAMES_DIR + "/" + file));
 
+      // Creates canvas with colored lines
       const canvas = createCanvasLines(colors);
-      res.json({ url: canvas.toDataURL("image/jpeg") });
+
+      // Return dataURL to client
+      res.json({ image: canvas.toDataURL("image/jpeg") });
     } catch (error) {
+      // Clear listener on error
+      clearDirInvteral();
+
       return next(createHttpError(400, error.message));
     }
   });
