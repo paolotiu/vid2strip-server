974b6c8e3bfcd19d6d24cdc4cfe1c25d769da66e
Paolo Tiu <paolotiu17@gmail.com>
2021-03-03 15:34:34 +0800

    removed console log

--- src/util/getSocket.ts
+++ src/util/getSocket.ts
@@ -6,8 +6,10 @@ export type Emitter = (event: string, payload: any) => boolean;
 export const getSocket = (req: Request, id: string): Emitter | null => {
   const io = req.app.get("io") as Server;
   const sockets = req.app.get("sockets") as Sockets;
+  if (!sockets) {
+    return null;
+  }
   const socketId = sockets[id];
-  console.log(socketId);
 
   return socketId ? (event: string, payload: any) => io.to(socketId).emit(event, payload) : null;
 };
