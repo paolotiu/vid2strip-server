23140bdd4af0c0e62645655a2f91cbad4dc0b557
Paolo Tiu <paolotiu17@gmail.com>
2021-03-03 15:18:07 +0800

    type update

--- types/color-thief-node/index.d.ts
+++ types/color-thief-node/index.d.ts
@@ -1,5 +1,5 @@
 declare module "color-thief-node" {
-  type Color = [number, number, number];
+  type Color = [number, number, number, number] | [number, number, number];
   const getColorFromURL: (path: string) => Promise<Color>;
   export const getColorFromURL;
 }
