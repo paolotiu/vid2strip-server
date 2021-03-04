bd2abf0f9fa9e2ceb7eb196069cdf88685a68868
Paolo Tiu <paolotiu17@gmail.com>
2021-03-04 16:18:55 +0800

    cleanup

--- src/util/getMultipleColors.ts
+++ src/util/getMultipleColors.ts
@@ -1,17 +1,14 @@
 import { eachLimit } from "async/";
 import { getAverageColor } from "fast-average-color-node";
 export const getColorFromFiles = async (filepaths: string[]) => {
-  // TODO: find a more efficient way to implment this.
-  // The Promise.all() trick overloads the memory so no-go on that.
-
   const colors: IFastAverageColorRgba[] = [];
   await eachLimit(filepaths, 20, async (file, cb) => {
-    // const color = await getColorFromURL(file);
+    // Inside setTimeout to allow other requests to come in.
     setTimeout(async () => {
       const color = await getAverageColor(file);
-      // const color: [number, number, number, number] = [0, 0, 0, 0];
+
       colors.push(color.value);
-      console.log(colors.length);
+
       cb();
     }, 0);
   });
